8d9a81e284b9baac189eb3bc6a839452c744a33d
Santiago Aguilera <santiago.aguilera@mercadolibre.com>
2020-12-03 20:19:34 -0300

    Upload code to github

---
+++ README.md
@@ -0,0 +1,10 @@
+# RETE Algorithm
+
+Simple RETE algorithm for FIUBA's 75.69 Fallas II
+
+## Run
+
+It's required to have Python 3 installed
+
+* pip install -r requirements.txt
+* python main.py
---
+++ rule.py
@@ -0,0 +1,17 @@
+class Rule(object):
+    def __init__(self, inputs, outputs):
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def applies(self, data):
+        a = set(self.inputs).issubset(set(data))
+        return a
+
+    def implies(self, target):
+        return target in self.outputs
+
+    def apply(self, knowledge):
+        knowledge.extend(self.outputs)
+    
+    def __repr__(self):
+        return ', '.join(['{key}={value}'.format(key=key, value=self.__dict__.get(key)) for key in self.__dict__])
